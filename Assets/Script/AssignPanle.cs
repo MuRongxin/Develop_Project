debc95160affa172ae08846064bf66095cbd7cec
MuRongxin <unity_l@qq.com>
2019-11-19 21:41:28 +0800

    No Position

--- Assets/Script/AssignPanle.cs
+++ Assets/Script/AssignPanle.cs
@@ -9,13 +9,16 @@ public class AssignPanle : MonoBehaviour {
 	[HideInInspector]
 	public static InputField iniputFirstName;
 	public static bool isNormalFirstName;
+	public static int lastnameCount;
+	public static string lastNameType;
 	private GameObject firstNameType;
 	private RectTransform toggleMove;
 	
 	public Toggle toggle_FirstNameType;
 	public ToggleGroup toggle_lastNameType;
-
-	public Toggle toggle_lastNameCount;
+	IEnumerable<Toggle> toggleGrouplastNameType;
+	public ToggleGroup toggle_lastNameCount;
+	IEnumerable<Toggle> toggleGrouplastNameCount;
 	public static bool isAssignFirstName;
 	private bool isMove;
 
@@ -29,6 +32,7 @@ public class AssignPanle : MonoBehaviour {
 		toggleMove=transform.Find("ToggleMove").GetComponent<RectTransform>();
 		beggin.onClick.AddListener(UIControl._instant.ResoultPanle);
 		
+
 	}
 	
 	// Update is called once per frame
@@ -56,6 +60,44 @@ public class AssignPanle : MonoBehaviour {
 		else
 			isNormalFirstName=false;
 
+		toggleGrouplastNameType=toggle_lastNameType.ActiveToggles();
+		
+		foreach(Toggle item in toggleGrouplastNameType)
+		{
+			if(item.isOn){
+				switch(item.name){
+					case "manToggle":
+					 lastNameType="boy";
+					 break;
+					 case "middleToggle":
+					 lastNameType="middle";
+					break;
+					case "wumenToggle":
+					lastNameType="girl";
+					break;
+				}
+				//Debug.Log(item.name+lastNameType);
+			}			
+		}
+
+		toggleGrouplastNameCount=toggle_lastNameCount.ActiveToggles();
+		foreach(var item in toggleGrouplastNameCount){
+			if(item.isOn)
+			{
+				switch(item.name){
+					case "aloneToggle":
+					 lastnameCount=1;
+					 break;
+					 case "doubleToggle":
+					 lastnameCount=2;
+					break;
+					 case "threeToggle":
+					 lastnameCount=3;
+					break;
+				}
+				//Debug.Log(item.name+lastnameCount);
+			}
+		}
 		
 	}
 	
